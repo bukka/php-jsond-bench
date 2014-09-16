5e6db3acee041b44694a8c4a2c9cac89ec4196c7
Jakub Zelenka <bukka@php.net>
2014-09-16 20:21:01 +0100

    Add empty generator

--- utils/console.php
+++ utils/console.php
@@ -1,16 +1,25 @@
 #!/usr/bin/env php
 <?php
 
-use Json\Bench\Conf;
-use Json\Bench\Bench;
-
 require_once __DIR__ . '/conf.php';
 require_once __DIR__ . '/bench.php';
+require_once __DIR__ . '/generator.php';
+
+use Json\Bench\Conf;
+use Json\Bench\Bench;
+use Json\Bench\Generator;
 
 $templateDir = __DIR__ . "/../templates/";
 $confDir = __DIR__ . "/../conf/";
 $confFile = $confDir . "bench.json";
 
 $conf = new Conf($confFile);
-$bench = new Bench($conf);
-$bench->run();
+
+if ($argc > 1 && strpos($argv[1], 'g') === 0) {
+	$gen = new Generator($conf);
+	echo 'te';
+	$gen->run();
+} else {
+	$bench = new Bench($conf);
+	$bench->run();
+}
---
+++ utils/generator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Json\Bench;
+
+use Json\Bench\Conf;
+
+class Generator
+{
+	protected $conf;
+	
+	public function __construct(Conf $conf) {
+		$this->conf = $conf;
+	}
+	
+	public function run() {
+		
+	}
+}
