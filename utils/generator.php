bd7f94cd4400f72f4c8cad1ac45e5e3b7662c9e4
Jakub Zelenka <bukka@php.net>
2014-10-03 20:29:24 +0100

    Add paths generation and clearing

--- utils/generator.php
+++ utils/generator.php
@@ -60,8 +60,60 @@ class Generator
 				}
 			}
 		} else {
-			$cmd = sprintf("%s %s -o %s -s %d", $this->conf->getGenerator(), $input, $output, $seed);
-			echo $cmd . "\n";
+			$filePaths = $this->createPaths($output, $seed, $count);
+			if (!empty($filePaths)) {
+				$this->clearExistingPaths($filePaths, $output, $count);
+				$seedValue = $seed;
+				foreach ($filePaths as $path) {
+					$cmd = sprintf("%s %s -o %s -s %d", $this->conf->getGenerator(), $input, $path, $seedValue++);
+					echo $cmd . "\n";
+				}
+			}
+		}
+	}
+
+	/**
+	 * Create paths for output
+	 *
+	 * @param string $output
+	 * @param int    $seed
+	 * @param int    $count
+	 *
+	 * @return array New paths
+	 */
+	protected function createPaths($output, $seed, $count) {
+		if ($count < 1) {
+			return array();
+		}
+		if ($count == 1) {
+			return array($output);
+		}
+		$nameWithoutExt = substr($output, 0, strlen($output) - 5);
+		$paths = array();
+		for ($i = 0; $i < $count; $i++) {
+			$paths[] = $nameWithoutExt . '__' . ($seed + $i) . '.json';
+		}
+		return $paths;
+	}
+
+	/**
+	 * Clear existing paths
+	 *
+	 * @param array  $newFilePaths
+	 * @param string $output
+	 * @param int    $count
+	 * @param int    $force
+	 */
+	protected function clearExistingPaths($newFilePaths, $output, $count, $force = false)
+	{
+		if (($force || $count > 1) && file_exists($output)) {
+			unlink($output);
+		}
+		$nameWithoutExt = substr($output, 0, strlen($output) - 5);
+		foreach (new \GlobIterator($nameWithoutExt . '__*') as $fileInfo) {
+			if ($force || !in_array($fileInfo->getPath(), $newFilePaths)) {
+				unlink($fileInfo->getPath());
+			}
 		}
 	}
 }
