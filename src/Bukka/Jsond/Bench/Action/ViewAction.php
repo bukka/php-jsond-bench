33701e2cf148929185615d1d9e6a64cd28692075
Jakub Zelenka <bukka@php.net>
2014-12-17 19:14:42 +0000

    Add stub for date view date processing

--- src/Bukka/Jsond/Bench/Action/ViewAction.php
+++ src/Bukka/Jsond/Bench/Action/ViewAction.php
@@ -8,9 +8,43 @@ namespace Bukka\Jsond\Bench\Action;
 class ViewAction extends AbstractAction
 {
     /**
+     * Processed data
+     *
+     * @var array
+     */
+    protected $data;
+
+    /**
      * Execute viewing of results
      */
     public function execute() {
-        
+        $this->loadDates();
+    }
+
+    /**
+     * Load all dates
+     */
+    protected function loadDates()
+    {
+        $this->data = array();
+        $dates = $this->conf->getParam('dates');
+        if (empty($dates)) {
+            // load last date
+            $this->loadDate();
+        } else {
+            foreach ($dates as $date) {
+                $this->loadDate($date);
+            }
+        }
+    }
+
+    /**
+     * Load single date
+     *
+     * @param string $date
+     */
+    protected function loadDate($date = null)
+    {
+
     }
 }
